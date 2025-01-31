d81875c1137daae9590748e956ed5a1545d626b8
Andrei Duvakin <andrei@duvakin.ru>
2025-01-31 19:31:43 +0500

    инициализировал проект api

---
+++ api/app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = "postgresql+asyncpg://user:password@localhost:5432/dbname"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
